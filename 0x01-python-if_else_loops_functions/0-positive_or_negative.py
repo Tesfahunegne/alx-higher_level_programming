771c13d6f573ee5330b633a1cc64061b92e1a070
Tesfahunenge <tsweetam@gmail.com>
2022-11-29 23:08:54 -0800

    my commit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,16 +2,30 @@
 
 import random
 
-number = random.randint(-10, 10)
+number = random.randint(-10000, 10000)
 
-if (number > 0):
+str1 = " and is greater than 5"
 
-        print("{} is positive".format(number))
+str2 = " and is 0"
 
-    elif (number == 0):
+str3 = " and is less than 6 and not 0"
 
-            print("{} is zero".format(number))
+if number < 0:
 
-        elif (number < 0):
+        last = number % -10
 
-                print("{} is negative".format(number))
+    else:
+
+            last = number % 10
+
+            if last > 5:
+
+                    print("Last digit of {} is {}".format(number, last) + str1)
+
+                elif last == 0:
+
+                        print("Last digit of {} is {}".format(number, last) + str2)
+
+                    else:
+
+                            print("Last digit of {} is {}".format(number, last) + str3)
