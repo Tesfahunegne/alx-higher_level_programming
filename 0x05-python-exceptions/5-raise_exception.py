f9c0ba25bfa67805c37d22c949d1192b15439b02
Tesfahunenge <tsweetam@gmail.com>
2022-12-28 04:18:16 -0800

    my commit

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+
+# 5-raise_exception.py
+
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+
+
+
 def raise_exception():
-raise TypeError
+
+        """Raise a TypeError exception."""
+
+            raise TypeError
