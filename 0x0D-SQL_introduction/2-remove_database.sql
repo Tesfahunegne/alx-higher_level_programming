987653ea8382592d4276d0c33158527e90cfd70e
Tesfahunegne <tsweetam@gmail.com>
2023-05-15 23:45:25 -0700

    my commit

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,6 +1,3 @@
--- Updates the score of a record in the
--- table `second_table` in MySQL Server.
-
-UPDATE second_table
-SET `score` = '10'
-WHERE `name` = 'Bob';
+-- Deletes the database `hbtn_0c_0` in MySQL Server.
+--      If the database `hbtn_0c_0` doesn't exists, the script shouldn't fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
